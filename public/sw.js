d9bf60d71411cd7dd3f03d6ec855705ad60cdb47
sayeed2758 <sayeedbackupmail@gmail.com>
2026-09-25 09:52:14 +0530

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'sayeed-courses-ui-v1';
+const APP_SHELL = ['/', '/manifest.webmanifest', '/shahid-logo.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request).catch(() => caches.match('/')))
+  );
+});
